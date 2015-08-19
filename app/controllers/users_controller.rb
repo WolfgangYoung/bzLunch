5a18c246ec72a84ceb6ea1acf0a90292bb2dd2b0
lionelyoung <lionelyoung@lionelyoungdeMacBook-Pro.local>
2015-08-19 20:39:43 +0800

    singup complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+
+  def show
+    @user = User.find(params[:id])
+    # debugger
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "欢迎来到百助订餐系统"
+      redirect_to user_url(@user)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
